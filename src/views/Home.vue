b2030230efa7ce71255eb2a6ae028cab1266acb5
Vincent Dante <vincent.dante96@gmail.com>
2021-04-06 02:56:42 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,23 +6,51 @@
         <input type="text" class="form-control" @keyup.enter="loadData()" v-model="search" placeholder="Search here...">
         <button class="btn btn-dark" @click="loadData()">Search</button>  
       </div>
+
     </div>
   </div>
 
+  <br>
+  <br>
+  <br>
+  <br>
+
+
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <div v-for="(obj, id) in datas" :key="id">
+        <div class="card mb-3" v-for="(link, id) in obj.links" :key="id">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img :src="link.href" alt="" srcset="" class="card-image">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body text-start">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">ff</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    </div>
 
 
 
-  <div class="row" data-masonry='{"percentPosition": true }'>
-    <div class="col-sm-6 col-lg-3 mb-3" v-for="(obj, id) in datas" :key="id">
+
+
+
+<!--     <div class="col-lg-12 mb-3" >
       <div class="card" v-for="(link, id) in obj.links" :key="id">
-        <img :src="link.href" alt="" srcset="" class="img-fluid">
+        <img :src="link.href" alt="" srcset="" class="card-image">
         <div class="card-body">
             ff
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
-
 
 
 
@@ -34,6 +62,7 @@
 
 <script>
 const axios = require('axios');
+
 
 export default {
   name: 'Home',
@@ -81,6 +110,10 @@ export default {
 
 .box-container {
   padding: 10px;
+}
+
+.card-image {
+  width: 100%;
 }
 
 .column-flex {
