994af211904379f3b9828bf159488d0be5cc8ecf
Vincent Dante <vincent.dante96@gmail.com>
2021-04-16 05:03:11 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,40 +43,39 @@
         </div>
       </masonry>
 
-      <div v-if="showModal" @close="showModal = false">
-        <transition name="modal">
-          <div class="modal-mask">
-            <div class="modal-wrapper">
-              <div class="modal-container">
 
-                <div class="modal-header">
-                  <slot name="header">
-                    default header
-                  </slot>
-                </div>
+      <v-dialog v-model="dialog" width="600px">
+        <div v-if="dialog">
+          <v-card>
 
-                <div class="modal-body">
-                  <slot name="body">
-                    {{ dataSelected.data[0].title }}
 
-                    <v-img lazy-src="https://picsum.photos/id/11/10/6" :src="dataSelected.links[0].href" class="card-image"></v-img> 
-                  </slot>
-                </div>
+            <v-img lazy-src="https://picsum.photos/id/11/10/6" :src="dataSelected.links[0].href" class="card-image"></v-img> 
 
-                <div class="modal-footer">
-                  <slot name="footer">
-                    default footer
-                    <button class="modal-default-button" @click="showModal = false">
-                      OK
-                    </button>
-                  </slot>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </transition>     
-      </div> 
+            <v-card-title>
+              <span class="headline">{{ dataSelected.data[0].title }}</span>
+            </v-card-title>
+
+
+            <v-card-text class="card-description">
+              {{ dataSelected.data[0].description }}
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="dialog = false"
+              >
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-dialog>
+
+
+
+
 
     </v-col>
   </v-row>
@@ -96,7 +95,8 @@ export default {
         value => !!value || 'Required.'
       ],
       showModal: false,
-      dataSelected: {}
+      dataSelected: {},
+      dialog: false
     }
   },
   methods: {
@@ -119,7 +119,7 @@ export default {
 
     },
     loadModal(obj){
-      this.showModal = true;
+      this.dialog = true;
       this.dataSelected = obj;
 
 
@@ -174,72 +174,8 @@ export default {
 }
 
 .card-description {
-  max-height: 155px;
-  overflow-y: hidden;
-}
-
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.65);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
   text-align: left;
-  width: 60%;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #1a2837;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-  font-family: Helvetica, Arial, sans-serif;
 }
 
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
 
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-.modal-enter {
-  opacity: 0;
-}
-
-.modal-leave-active {
-  opacity: 0;
-}
-
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
 </style>
